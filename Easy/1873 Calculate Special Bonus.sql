3c7db088e59a1f65a1876fb33adf555fb587c7b5
Rohan Baravkar <60055447+rohanbaravkar4@users.noreply.github.com>
2023-01-16 18:26:53 -0800

    Update 1873 Calculate Special Bonus.sql

--- Easy/1873 Calculate Special Bonus.sql
+++ Easy/1873 Calculate Special Bonus.sql
@@ -1,3 +1,4 @@
+'''
 -- 1873. Calculate Special Bonus
 
 -- SQL Schema
@@ -24,6 +25,8 @@
 SELECT employee_id,
 IF (employee_id % 2 != 0 AND name NOT LIKE 'M%', salary, 0) as bonus
 FROM Employees ORDER BY employee_id;
+'''
+
 
 -- Solution 2
 SELECT employee_id,
