061c03755e0ddefa572da6fb233baba08522a439
Rohan Baravkar <60055447+rohanbaravkar4@users.noreply.github.com>
2023-01-16 18:30:39 -0800

    Update 627. Swap Salary.sql

--- Easy/627. Swap Salary.sql
+++ Easy/627. Swap Salary.sql
@@ -1,4 +1,4 @@
-
+'''
 Q. 
 
 +-------------+----------+
@@ -47,6 +47,9 @@ Explanation:
 (1, A) and (3, C) were changed from 'm' to 'f'.
 (2, B) and (4, D) were changed from 'f' to 'm'.
 +==================================================================================================================================
+'''
+
+
 
 # Write your MySQL query statement below
 Update Salary
